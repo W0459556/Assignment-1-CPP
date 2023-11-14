270f512ba218254bcb9bcb23315a4ad6ba8e795a
Breanna Young <w0459556@campus.nscc.ca>
2023-11-14 13:09:40 -0400

    finished q1 hipsters local vinyl records

--- q1_hipstersLocalVinylRecords.cpp
+++ q1_hipstersLocalVinylRecords.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#define fixedFloat(x) fixed << setprecision(2) << (x) // float of 2 decimal places
 using namespace std;
+
+
+struct{
+    string custName;
+    float distKilometers;
+    float totCost;
+} receipt;
+
 
 int main() {
 
-  return 0;
+    cout << "Hipster's Local Vinyl Records - Customer Order Details\n\n";
+
+    cout << "Enter the customer's name: ";
+    getline (cin, receipt.custName);
+    // test: cout << "\n" << receipt.custName << "\n";
+
+    cout << "Enter the distance in kilometers for delivery: ";
+    cin >> receipt.distKilometers;
+
+    cout << "Enter the cost of records purcased: ";
+    cin >> receipt.totCost;
+
+    struct{
+        float delivCost = receipt.distKilometers*15;
+        float purcaseCost = receipt.totCost*1.14;
+    } endCosts;
+
+    cout << "\n\nPurchase summary for " << receipt.custName << "\n";
+    cout << "Delivery Cost: $" << fixedFloat(endCosts.delivCost) << "\n";
+    cout << "Purchase Cost: $" << fixedFloat(endCosts.purcaseCost) << "\n";
+    cout << "Total Cost:    $" << fixedFloat(endCosts.delivCost+endCosts.purcaseCost) << "\n";
+
+    return 0;
 }
