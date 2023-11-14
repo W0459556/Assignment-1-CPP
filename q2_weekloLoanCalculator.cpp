1097ad34cc913bc255b7828ec3b53e553cef32b5
Breanna Young <w0459556@campus.nscc.ca>
2023-11-14 15:17:02 -0400

    finished q2, moved out files to dedicated folder

--- q2_weekloLoanCalculator.cpp
+++ q2_weekloLoanCalculator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cmath> 
 #define fixedFloat(x) fixed << setprecision(2) << (x) // float of 2 decimal places
 using namespace std;
 
@@ -16,16 +17,31 @@ int main() {
     cout << "Weekly Loan Calculator\n\n";
 
     cout << "Enter the amount of loan: $";
-    cin >> loanInputs.loanAmount
+    cin >> loanInputs.loanAmount;
 
-    cout <<"\nEnter the interest rate (%): "
+    cout <<"Enter the interest rate (%): ";
     cin >> loanInputs.interestRate;
 
-    cout << "\nEnter the number of years: ";
+    cout << "Enter the number of years: ";
     cin >> loanInputs.yearsAmount;
 
-    cout << "\nYour weekly payment will be: $"
+    double topOfFraction = loanInputs.interestRate/5200;
+    // cout << topOfFraction << "\n";
 
+    double bottomOfFractionLeft = (1 + topOfFraction);
+    // cout << bottomOfFractionLeft << "\n";
+
+    double bottomOfFractionRight = -52*loanInputs.yearsAmount;
+    // cout << bottomOfFractionRight << "\n";
+
+    double bottomOfFraction = 1 - pow(bottomOfFractionLeft, bottomOfFractionRight);
+    // cout << bottomOfFraction << "\n";
+
+    float weeklyLoanPayment = (topOfFraction/bottomOfFraction) * loanInputs.loanAmount;
+    // cout << weeklyLoanPayment << "\n";
+
+
+    cout << "\nYour weekly payment will be: $" << fixedFloat(weeklyLoanPayment) << "\n";
 
     return 0;
 }
